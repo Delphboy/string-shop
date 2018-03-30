0eae54f3d948a64ee59275ddd23db2941137e2f7
Henry <hps1945@live.co.uk>
2018-03-30 21:54:21 +0100

    Added formatting to livesearch

--- livesearch.php
+++ livesearch.php
@@ -11,6 +11,8 @@ $a[] = "Amy";
 $a[] = "Bilyana";
 $a[] = "Bill";
 $a[] = "Charlie";
+$a[] = "Charles";
+$a[] = "Henry";
 
 $query = $_REQUEST['q'];
 $hint = "";
@@ -23,9 +25,21 @@ if($query !== "")
         if(stristr($query, substr($name, 0, $len)))
         {
             if($hint === "")
-                $hint = "<li class=\"list-group-item\">$name</li>";
+                $hint = "<li class='list-group-item container'>
+                                <div class='col-md-1'>
+                                    <img src='http://www.hondahookup.com/images/100x100.jpg' style='width: 50px; height: auto' />
+                                </div>
+                                <div class='col-md-9 list-group-item-text'><h4>$name</h4></div>
+                                <div class='col-md-9'>$300</div>
+                         </li>";
             else
-                $hint .= "<li class=\"list-group-item\">$name</li>";
+                $hint .= "<li class='list-group-item container'>
+                                <div class='col-md-1'>
+                                    <img src='http://www.hondahookup.com/images/100x100.jpg' style='width: 50px; height: auto' />
+                                </div>
+                                <div class='col-md-9 list-group-item-text'><h4>$name</h4></div>
+                                <div class='col-md-9'>$300</div>
+                         </li>";
         }
     }
 }
