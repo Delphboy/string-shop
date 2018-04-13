7876b8b68bcf0cde30a4a0ea4313f048f039d72c
Henry <hps1945@live.co.uk>
2018-04-13 13:11:14 +0100

    Live search now works on n-Dimensional Arrays

--- livesearch.php
+++ livesearch.php
@@ -5,40 +5,43 @@
  * Date: 30/03/2018
  * Time: 19:56
  */
-
-$a[] = "Anna";
-$a[] = "Amy";
-$a[] = "Bilyana";
-$a[] = "Bill";
-$a[] = "Charlie";
-$a[] = "Charles";
-$a[] = "Henry";
+$items = array
+(
+    array("Anna", "images/uploads/no-image.png", "£150"),
+    array("Amy", "http://www.hondahookup.com/images/100x100.jpg", "£300"),
+    array("Bill", "http://www.hondahookup.com/images/100x100.jpg", "£300"),
+    array("Ben", "http://www.hondahookup.com/images/100x100.jpg", "£300"),
+    array("Charles", "http://www.hondahookup.com/images/100x100.jpg", "£300"),
+    array("Charlie", "http://www.hondahookup.com/images/100x100.jpg", "£300"),
+    array("Henry", "images/advert-pictures/expensive-fullsize-strings.jpg", "£700"),
+);
 
 $query = $_REQUEST['q'];
+
 $hint = "";
 if($query !== "")
 {
     $query = strtolower($query);
     $len = strlen($query);
-    foreach ($a as $name)
+    for ($i = 0; $i < sizeof($items); $i++)
     {
-        if(stristr($query, substr($name, 0, $len)))
+        if(stristr($query, substr($items[$i][0], 0, $len)))
         {
             if($hint === "")
                 $hint = "<li class='list-group-item container'>
                                 <div class='col-md-1'>
-                                    <img src='http://www.hondahookup.com/images/100x100.jpg' style='width: 50px; height: auto' />
+                                    <img src='" . $items[$i][1] . "' style='width: 75px; height: auto' />
                                 </div>
-                                <div class='col-md-9 list-group-item-text'><h4>$name</h4></div>
-                                <div class='col-md-9'>$300</div>
+                                <div class='col-md-9 list-group-item-text'><h4>" . $items[$i][0] . "</h4></div>
+                                <div class='col-md-9'>" . $items[$i][2] . "</div>
                          </li>";
             else
                 $hint .= "<li class='list-group-item container'>
                                 <div class='col-md-1'>
-                                    <img src='http://www.hondahookup.com/images/100x100.jpg' style='width: 50px; height: auto' />
+                                    <img src='" . $items[$i][1] . "' style='width: 75px; height: auto' />
                                 </div>
-                                <div class='col-md-9 list-group-item-text'><h4>$name</h4></div>
-                                <div class='col-md-9'>$300</div>
+                                <div class='col-md-9 list-group-item-text'><h4>" . $items[$i][0] . "</h4></div>
+                                <div class='col-md-9'>" . $items[$i][2] . "</div>
                          </li>";
         }
     }
